4bb7a69a52b9289bab04f7d17404dbf53084f37d
Leslie Stevens <flylady@mail.utexas.edu>
2018-04-13 21:09:53 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  new_numbers = array.each do|x|
-    x**x
+  new_array = []
+  array.each do|x|
+    new_array.push(x**2)
   end
-  # your code here
+  new_array# your code here
 end
